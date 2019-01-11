cc877afafe757beb15bb96b5f2a10473e5ded6d2
Conor <cwilson0005@hotmail.co.uk>
2019-01-11 10:23:41 +0000

    First commit

---
+++ db/cinema.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS tickets;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS films;
+DROP TABLE IF EXISTS screenings;
+
+CREATE TABLE customers(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  funds INT4
+);
+
+CREATE TABLE films(
+  id SERIAL4 PRIMARY KEY,
+  title VARCHAR(255),
+  price INT4
+);
+
+CREATE TABLE screenings(
+  id SERIAL4 PRIMARY KEY,
+  show_time VARCHAR(255)
+);
+
+CREATE TABLE tickets(
+  id SERIAL4 PRIMARY KEY,
+  customer_id INT4 REFERENCES customers.(id) ON DELETE CASCADE,
+  film_id INT4 REFERENCES films.(id) ON DELETE CASCADE,
+  screen_id INT4 REFERENCES screenings.(id) ON DELETE CASCADE
+);
